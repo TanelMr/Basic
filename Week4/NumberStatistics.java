e739ae763215651eb9d51e6140e6b63cc2bbda25
Tanel Märjama <tanel.marjama@khk.ee>
2022-09-09 11:56:18 +0300

    Exercise 79.2 week 4

--- Week4/NumberStatistics.java
+++ Week4/NumberStatistics.java
@@ -3,20 +3,31 @@ package Week4;
 public class NumberStatistics {
 
     private int amountOfNumbers;
+    private int sum;
 
     public NumberStatistics() {
         // initialize here the object variable amountOfNumbers
         this.amountOfNumbers = 0;
+        this.sum = 0;
 
     }
 
     public void addNumber(int number) {
-       this.amountOfNumbers++;
+        this.sum += number;
+        this.amountOfNumbers++;
 
     }
 
     public int amountOfNumbers() {
         return this.amountOfNumbers;
+    }
+
+    public int sum() {
+        return this.sum;
+    }
+
+    public double average() {
+        return (double)this.sum/this.amountOfNumbers;
     }
 
 }
