8fb5f6ed26f47c2e53badf60086b09f6d1322c69
Tanel Märjama <tanel.marjama@khk.ee>
2022-09-09 11:35:56 +0300

    Exercise 79.1 week 4

---
+++ Week4/NumberStatistics.java
@@ -0,0 +1,22 @@
+package Week4;
+
+public class NumberStatistics {
+
+    private int amountOfNumbers;
+
+    public NumberStatistics() {
+        // initialize here the object variable amountOfNumbers
+        this.amountOfNumbers = 0;
+
+    }
+
+    public void addNumber(int number) {
+       this.amountOfNumbers++;
+
+    }
+
+    public int amountOfNumbers() {
+        return this.amountOfNumbers;
+    }
+
+}
