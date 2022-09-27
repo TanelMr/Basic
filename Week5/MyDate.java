81f71177af804372b58f097689a6a3406f590785
Tanel Märjama <tanel.marjama@khk.ee>
2022-09-27 10:52:46 +0300

    Exercise 93 week 5

---
+++ Week5/MyDate.java
@@ -0,0 +1,24 @@
+package Week5;
+
+public class MyDate {
+    private int day;
+    private int month;
+    private int year;
+
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int setYear() {
+        return this.year;
+    }
+
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+
+
+}
