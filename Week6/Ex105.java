1a61fb96c7fb3d41f3f51a4b929998e692b28632
Tanel Märjama <tanel.marjama@khk.ee>
2022-10-02 18:40:02 +0300

    Exercise 105.1 week 6

---
+++ Week6/Ex105.java
@@ -0,0 +1,10 @@
+package Week6;
+
+public class Ex105 {
+    public static void main(String[] args) {
+
+        GuessingGame game = new GuessingGame();
+        System.out.println(game.isGreaterThan(32));
+
+    }
+}
---
+++ Week6/GuessingGame.java
@@ -0,0 +1,37 @@
+package Week6;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class GuessingGame {
+    private Scanner reader;
+
+    public GuessingGame() {
+        this.reader = new Scanner(System.in);
+    }
+
+    public void play(int lowerLimit, int upperLimit) {
+        instructions(upperLimit, lowerLimit);
+    }
+
+    public boolean isGreaterThan(int value){
+        System.out.println("Is your number greater than given value? (y/n)");
+        String input = reader.nextLine();
+        return Objects.equals(input, "y");
+    }
+
+    public void instructions(int lowerLimit, int upperLimit) {
+        int maxQuestions = howManyTimesHalvable(upperLimit - lowerLimit);
+
+        System.out.println("Think of a number between " + lowerLimit + "..." + upperLimit + ".");
+
+        System.out.println("I promise you that I can guess the number you are thinking of with " + maxQuestions + " questions.");
+        System.out.println("");
+        System.out.println("Next I'll present you with a series of questions. Answer them honestly.");
+        System.out.println("");
+    }
+    public static int howManyTimesHalvable(int number) {
+        return (int) (Math.log(number) / Math.log(2)) + 1;
+    }
+}
+
